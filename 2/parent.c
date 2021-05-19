20a4714d0a0bb5abd64449512dcecb7bfd61b2bc
Toan Nguyen <toan.nguyen@rwth-aachen.de>
2021-05-19 17:22:07 +0700

    Aufgabe 4a

--- 2/parent.c
+++ 2/parent.c
@@ -7,10 +7,16 @@
 extern char **environ; // standard libc process environment
 
 int main(void) {
-  pid_t child1;
+  pid_t child1, child2;
   char const* args[] = { "./child1", NULL };
+  char const* args2[] = { "./child2", NULL };
   int ret = posix_spawn(&child1, *args, NULL, NULL, (char* const*)args, environ);
+  int ret2 = posix_spawn(&child2, *args2, NULL, NULL, (char* const*)args2, environ);
   if(ret != 0) {
+    fprintf(stderr, "Could not start child process!\n");
+    abort();
+  }
+  if(ret2 != 0) {
     fprintf(stderr, "Could not start child process!\n");
     abort();
   }
